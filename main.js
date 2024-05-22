b04714afdee7784a7f0e41396e6f5ea60754e763
Dao Dung <100118002+DaoDung2005@users.noreply.github.com>
2024-05-23 00:17:17 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,90 +1,105 @@
-var YouTube = document.querySelector('.YouTube');
-var idList = document.querySelector('.idList');
-var hidden = document.querySelector('.hidden');
-function Get(){
-  if(YouTube.value.indexOf("https://youtu.be/") != -1){
-               var YTurl = YouTube.value.replace("https://youtu.be/","");
-               document.querySelector('.idList').value = YTurl;
-               Music();
-             }
-             else if(YouTube.value.indexOf("https://www.youtube.com/watch?v=") != -1){
-                var YTurl = YouTube.value.replace("https://www.youtube.com/watch?v=","");
-                document.querySelector('.idList').value = YTurl;
-                Music();
-                }
-             else if(YouTube.value.indexOf("https://youtube.com/watch?v=") != -1){
-                var YTurl = YouTube.value.replace("https://youtube.com/watch?v=","");
-                document.querySelector('.idList').value = YTurl;
-                Music();
-              }
-           
-              else if(YouTube.value.indexOf("https://m.youtube.com/watch?v=") != -1){
-                var YTurl = YouTube.value.replace("https://m.youtube.com/watch?v=","");
-                document.querySelector('.idList').value = YTurl;
-                Music();
-              }
-             else if(YouTube.value.indexOf("https://youtube.com/shorts/") != -1){
-               var YTurl = YouTube.value.replace("https://youtube.com/shorts/","");
-               var YTurl2 = YTurl.replace("?feature=share","");
-               document.querySelector('.idList').value = YTurl2;
-               Music();
-             }
-             else{
-               alert('Enter Your YouTube Url...');
-             }
-        }
-function Music() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'd24e43945cmshda3cd58fdbe3e78p110606jsnb2350bf37f51',
-            'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
-        }
-    };
 
-    var urlLink = 'https://youtube-mp36.p.rapidapi.com/dl?id=' + idList.value;
-
-    // Lấy thông tin về video từ YouTube Data API
-    
-
-    // Tiếp tục như thông thường để lấy link download
-    fetch(urlLink, options)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            hidden.classList.add('active');
-            document.querySelector('.form-control').value = data.link;
-        })
-        .catch(err => console.error(err));
-
-        fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + idList.value + '&key=AIzaSyCut28qePs8i1Il-MllszWkOMQb2x1ttpw')
-        .then(response => response.json())
-        .then(data => {
-            // Kiểm tra hình ảnh maxres
-            let thumbnailUrl = data.items[0].snippet.thumbnails.maxres.url;
-            if (!thumbnailUrl) {
-                // Nếu không có hình ảnh maxres, thử sử dụng hình ảnh high
-                thumbnailUrl = data.items[0].snippet.thumbnails.high.url;
-                if (!thumbnailUrl) {
-                    // Nếu không có hình ảnh high, sử dụng hình ảnh mặc định
-                    thumbnailUrl = data.items[0].snippet.thumbnails.default.url;
-                }
-            }
-            document.querySelector('.video-thumbnail').src = thumbnailUrl;
-        })
-        .catch(err => console.error(err));
-
-
-
-}
-var url = document.querySelector('.form-control');
-function download(){
+  var YouTube = document.querySelector('.YouTube');
+  var YouTube2 = document.querySelector('.YouTube2');
+  var idList = document.querySelector('.idList');
+  var idList2 = document.querySelector('.idList2');
+  var hidden = document.querySelector('.hidden');
+  var hidden2 = document.querySelector('.hidden2');
+  
+  function Get() {
+      processUrl(YouTube.value, idList, hidden, '.form-control', '.video-thumbnail', '.video-title');
+      processUrl(YouTube2.value, idList2, hidden2, '.form-control2', '.video-thumbnail2', '.video-title2');
+  }
+  
+  function processUrl(url, idElement, hiddenElement, formControlSelector, thumbnailSelector, titleSelector) {
+      var YTurl = '';
+  
+      if (url.indexOf("https://youtu.be/") !== -1) {
+          YTurl = url.replace("https://youtu.be/", "");
+      } else if (url.indexOf("https://www.youtube.com/watch?v=") !== -1) {
+          YTurl = url.replace("https://www.youtube.com/watch?v=", "");
+      } else if (url.indexOf("https://youtube.com/watch?v=") !== -1) {
+          YTurl = url.replace("https://youtube.com/watch?v=", "");
+      } else if (url.indexOf("https://m.youtube.com/watch?v=") !== -1) {
+          YTurl = url.replace("https://m.youtube.com/watch?v=", "");
+      } else if (url.indexOf("https://youtube.com/shorts/") !== -1) {
+          YTurl = url.replace("https://youtube.com/shorts/", "").replace("?feature=share", "");
+      } else {
+          alert('Enter a valid YouTube URL...');
+          return;
+      }
+  
+      idElement.value = YTurl;
+      fetchMusicData(YTurl, hiddenElement, formControlSelector, thumbnailSelector, titleSelector);
+  }
+  
+  function fetchMusicData(YTurl, hiddenElement, formControlSelector, thumbnailSelector, titleSelector) {
+      const options = {
+          method: 'GET',
+          headers: {
+              'X-RapidAPI-Key': 'd24e43945cmshda3cd58fdbe3e78p110606jsnb2350bf37f51',
+              'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+          }
+      };
+  
+      var urlLink = 'https://youtube-mp36.p.rapidapi.com/dl?id=' + YTurl;
+  
+      fetch(urlLink, options)
+          .then(response => response.json())
+          .then(data => {
+              console.log(data);
+              hiddenElement.classList.add('active');
+              document.querySelector(formControlSelector).value = data.link;
+          })
+          .catch(err => console.error(err));
+  
+      fetch('https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' + YTurl + '&key=AIzaSyCut28qePs8i1Il-MllszWkOMQb2x1ttpw')
+          .then(response => response.json())
+          .then(data => {
+              let videoTitle = data.items[0].snippet.title;
+              let thumbnailUrl = data.items[0].snippet.thumbnails.maxres?.url || 
+                                 data.items[0].snippet.thumbnails.high?.url || 
+                                 data.items[0].snippet.thumbnails.default?.url;
+              document.querySelector(thumbnailSelector).src = thumbnailUrl;
+              document.querySelector(titleSelector).textContent = videoTitle;
+          })
+          .catch(err => console.error(err));
+  }
+  
+  function download() {
+      const url = document.querySelector('.form-control').value;
       const anchor = document.createElement("a");
-      anchor.href =  url.value;
-      anchor.download = 'MWC.mp3';
+      anchor.href = url;
+      anchor.download = 'Music.mp3';
       document.body.appendChild(anchor);
       anchor.click();
       document.body.removeChild(anchor);
- }
+  }
+  
+  function download2() {
+      const url = document.querySelector('.form-control2').value;
+      const anchor = document.createElement("a");
+      anchor.href = url;
+      anchor.download = 'Music.mp3';
+      document.body.appendChild(anchor);
+      anchor.click();
+      document.body.removeChild(anchor);
+  }
+  function copyTextToClipboard() {
+    // Lấy giá trị từ trường nhập liệu
+    var textToCopy = document.getElementById("textToCopy").value;
+
+    // Tạo một phần tử textarea ẩn
+    var tempTextArea = document.createElement("textarea");
+    tempTextArea.value = textToCopy;
+    document.body.appendChild(tempTextArea);
+
+    // Lựa chọn toàn bộ nội dung của textarea
+    tempTextArea.select();
+
+    // Sao chép nội dung đã chọn vào bộ nhớ đệm
+    document.execCommand("copy");
+
+    // Xóa phần tử textarea ẩn
+    document.body.removeChild(tempTextArea);
+}
